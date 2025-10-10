fb6c59f94a88325a2ddc60039d82e4e9772c6fa9
lolo391 <lohana.monteiro@escola.pr.gov.br>
2025-10-10 08:03:05 -0300

    ....

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criacartao(
+    'programação',
+    'oque é python?',
+    'o python é uma linguagem de programação'
+)
+
+criacartao(
+    'geografia',
+    'qual a capital da frança?',
+    'a capital da frança é paris'
+)
+
+criacartao(
+    'programação',
+    'oque é uma função?',
+    'uma função é um bloco de codigo que excuta alguma tarefa'
+)
